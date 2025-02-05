e2d09c5277554193bbabf560609d69c007f545eb
NicoChlebo <nico.chlebo@gmail.com>
2025-02-05 09:01:26 +0100

    add file function2 + test function calculateTotalPrice

---
+++ test/function2.test.js
@@ -0,0 +1,38 @@
+const { isEven, calculateTotalPrice, processPurchase, sendNotification, generatePassword } = require("./../src/function.js");
+
+
+describe('Is this number even ?', () => {
+    test("Test number with a string",() =>{
+        expect(() => isEven('5')).toThrowError("Input must be a number")
+    });
+    test("Test number is not even", () => {
+        expect(isEven(5)).toBe(false)
+    })
+    test("Test number is even", () => {
+        expect(isEven(4)).toBe(true)
+    })
+})
+
+describe('Calculate total price of a cart, with tax added', () => {
+    test("Test prices are not an array",() =>{
+        expect(() => calculateTotalPrice(5, 10, 0.2)).toThrowError("Prices must be an array")
+    });
+    test("Test prices are an array",() =>{
+        expect(calculateTotalPrice([5, 10], 0.2)).toBe(18)
+    });
+    test("Test tax rate is not a number",() =>{
+        expect(() => calculateTotalPrice([5, 10], '0.2')).toThrowError("Tax rate must be a number")
+    });
+    test("Test tax rate is a number",() =>{
+        expect(calculateTotalPrice([5, 10], 0.2)).toBe(18)
+    });
+    test("Test each prices are not a number",() =>{
+        expect(() => calculateTotalPrice(['5', '10'], 0.2)).toThrowError("Each price must be a non-negative number")
+    });
+    test("Test each prices are negative",() =>{
+        expect(() => calculateTotalPrice([-5, -10], 0.2)).toThrowError("Each price must be a non-negative number")
+    });
+    test("Test each prices are non-negative number", () => {
+        expect(calculateTotalPrice([5, 10], 0.2)).toBe(18)
+    });
+})
