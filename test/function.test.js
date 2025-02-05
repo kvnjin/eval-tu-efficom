87bc4a7dbcada0715a0c454e39a669932cd0a8ed
Kévin Jin <kevin1501@hotmail.fr>
2025-02-05 08:33:05 +0100

    Add test isEven

--- test/function.test.js
+++ test/function.test.js
@@ -5,4 +5,10 @@ describe('Is this number even ?', () => {
     test("Test number with a string",() =>{
         expect(() => isEven('5')).toThrowError("Input must be a number")
     });
+    test("Test number is not even", () => {
+        expect(isEven(5)).toBe(false)
+    })
+    test("Test number is even", () => {
+        expect(isEven(4)).toBe(true)
+    })
 })
